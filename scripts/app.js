b7f5f3bad1adb0a8f261a1317bdffd7705aec081
John-Philip Johansson <seriema@gmail.com>
2013-11-18 10:17:24 +0100

    Updates

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,3 @@
 'use strict';
 
-angular.module('npmalerts', ['ngRoute', 'ui.bootstrap'])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'SignUpCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.module('npmalerts', ['ui.bootstrap']);
