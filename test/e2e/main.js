b558ecb51635fdc11c612d5231aff98805c6be68
John-Philip Johansson <seriema@gmail.com>
2013-12-04 21:41:01 +0100

    New design

---
+++ test/e2e/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Sign up page', function () {
+
+	var watchButton;
+
+	beforeEach(function () {
+		watchButton = element('.btn-primary');
+	});
+
+	it('should have Watch/Unwatch disabled by default', function() {
+		expect(watchButton.attr('disabled')).toBeTruthy();
+	});
+
+	it('should enable Watch/Unwatch button', function() {
+		input('#email').enter('banan@fluga.com');
+		expect(watchButton.attr('disabled')).toBeTruthy();
+
+		input('#url').enter('http://github.com');
+		expect(watchButton.attr('disabled')).toBeFalsy();
+	});
+});
